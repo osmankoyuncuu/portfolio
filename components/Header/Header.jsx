9e87f8e027ab69a3d71ffb26d0f31b66382497da
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-14 01:15:06 +0300

    add header content

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,5 +1,63 @@
+import React from "react";
+import { Container } from "reactstrap";
+import classes from "./header.module.css";
+import Link from "next/link";
+
+const NAV__LINK = [
+  {
+    path: "/",
+    display: "Home",
+  },
+  {
+    path: "#about",
+    display: "About",
+  },
+  {
+    path: "#services",
+    display: "Services",
+  },
+  {
+    path: "#portfolio",
+    display: "Portfolio",
+  },
+  {
+    path: "#contact",
+    display: "Contact",
+  },
+];
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={`${classes.header}`}>
+      <Container>
+        <div className={`${classes.nav__wrapper}`}>
+          {/*==============navigation logo===============*/}
+          <div className={`${classes.logo}`}>
+            <h1>
+              <span>O</span>sman
+            </h1>
+          </div>
+
+          {/*==============nav menu===============*/}
+          <div className={`${classes.navigation}`}>
+            <div className={`${classes.nav__menu}`}>
+              {NAV__LINK.map((item, index) => (
+                <Link href={item.path} key={index}>
+                  {item.display}
+                </Link>
+              ))}
+              <div className={`${classes.nav__right}`}>
+                <p className="d-flex align-items-center gap-2 mb-0">
+                  {" "}
+                  <i className="ri-phone-line"></i>0530 236 00 00{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
