6fc67d6ecad8a9b95c72fa0077db5fa472739d8a
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-17 01:21:23 +0300

    fixed services

---
+++ components/UI/Services.jsx
@@ -0,0 +1,40 @@
+import { Container, Row, Col } from "reactstrap";
+import classes from "../../styles/services.module.css";
+import SectionSubtitle from "./SectionSubtitle";
+import ServicesItem from "./ServicesItem";
+
+const Services = () => {
+  return (
+    <section id="services">
+      <Container>
+        <Row>
+          <Col md="6">
+            <div className={`${classes.services__container}`}>
+              <div>
+                <ServicesItem title="App Development" icon="ri-command-line" />
+                <ServicesItem title="Web Development" icon="ri-computer-line" />
+              </div>
+              <ServicesItem
+                title="Full-Stack Development"
+                icon="ri-code-s-slash-line"
+              />
+            </div>
+          </Col>
+          <Col md="6">
+            <SectionSubtitle subtitle="What I do" />
+            <h3 className="mb-0 mt-4">Better Desing,</h3>
+            <h3 className="mb-4">Better Experience</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              pariatur temporibus soluta necessitatibus minus amet tempora quis
+              harum natus voluptates eum exercitationem tempore, unde nulla,
+              eaque nisi culpa laborum. Reprehenderit.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Services;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Services from "../components/UI/Services";
 import Hero from "../components/UI/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
