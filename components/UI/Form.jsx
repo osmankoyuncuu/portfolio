0346b5540463099bd21a223c6fe88e9fe0dd1823
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-01-22 23:44:22 +0300

    add formspree

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -1,17 +1,40 @@
+import Link from "next/link";
 import classes from "../../styles/form.module.css";
+import { useRouter } from "next/router";
 const Form = () => {
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    //e.preventDefault();
+    setTimeout(() => {
+      router.push("/");
+    }, 500);
+  };
   return (
-    <form className={`${classes.form}`}>
+    <form
+      className={`${classes.form}`}
+      action="https://formspree.io/f/mnqynrnp"
+      method="POST"
+    >
       <div className={`${classes.from__group}`}>
-        <input type="text" placeholder="Your Name" required />
+        <input type="text" placeholder="Your Name" name="name" required />
       </div>
       <div className={`${classes.from__group}`}>
-        <input type="email" placeholder="Email Address" required />
+        <input type="email" placeholder="Email Address" name="email" required />
       </div>
       <div className={`${classes.from__group}`}>
-        <textarea type="text" row={10} placeholder="Message" required />
+        <textarea
+          type="text"
+          row={10}
+          placeholder="Message"
+          name="message"
+          required
+        />
       </div>
-      <button className="primary__btn" type="submit">
+      <button
+        className="primary__btn"
+        type="submit"
+        onClick={() => handleSubmit}
+      >
         Send
       </button>
     </form>
