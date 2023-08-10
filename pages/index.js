67267d2336a7d6ee8ee832b7e772cbf69ff594ca
osman <osmannnkoyuncuuu@gmail.com>
2023-08-10 12:21:02 +0300

    add google search

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,29 @@ import About from "../components/UI/About";
 import Portfolio from "../components/UI/Portfolio";
 import Testimonial from "../components/UI/Testimonial";
 import Contact from "../components/UI/Contact";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <Services />
-      <About />
-      <Portfolio />
-      <Testimonial />
-      <Contact />
+      <Head>
+        <title>Portfolio | Osman KOYUNCU</title>
+        <meta name="description" content="Rest Play" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="google-site-verification"
+          content="LLJiIX_X0gbF1leZqlLiyA8hX5eiVFAryTXXDNRuRsc"
+        />
+      </Head>
+      <main>
+        <Hero />
+        <Services />
+        <About />
+        <Portfolio />
+        <Testimonial />
+        <Contact />
+      </main>
     </>
   );
 }
